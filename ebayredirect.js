9944f9c9d79ed4cb78fabc0cc9c77a938a55fd83
Dec <decbrks@pm.me>
2026-06-22 22:40:55 +0100

    give user choice of redirect or not

--- ebayredirect.js
+++ ebayredirect.js
@@ -1,2 +1,9 @@
-alert("you are on ebay.com!")
-window.location.replace("https://ebay.co.uk");
+const url = window.location.href
+
+if (confirm("from ebayredirect extension\n\nyou are on ebay.com!\nclick 'okay' to redirect to ebay.co.uk\nclick 'cancel' to stay on ebay.com")) {
+    window.location.replace("https://ebay.co.uk/" + url.split("ebay.com/")[1]);
+} else {
+    // pass
+}
+
+// todo remember for this session.
